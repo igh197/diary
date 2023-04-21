643552930b85601e361f25a6e48ff4a706f85ac9
igh197 <57895194+igh197@users.noreply.github.com>
2023-04-21 16:19:04 +0900

    user repository class for DB connection

---
+++ main/java/computer/seoultech/diary/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package computer.seoultech.diary.repository;
+
+import computer.seoultech.diary.entity.User;
+import computer.seoultech.diary.network.Header;
+import computer.seoultech.diary.network.UserRequest;
+import computer.seoultech.diary.network.UserResponse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+public interface UserRepository extends JpaRepository<User,Long> {  //jpa를 이용하여 db에 접근하기 위한 인터페이스
+    Optional<User> findByAccount(String account);
+}
