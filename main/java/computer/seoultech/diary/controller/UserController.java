9f4a50c5396cd3f454ce138792ef18fc0811cff7
igh197 <57895194+igh197@users.noreply.github.com>
2023-05-01 10:46:17 +0900

    user controller final

--- main/java/computer/seoultech/diary/controller/UserController.java
+++ main/java/computer/seoultech/diary/controller/UserController.java
@@ -3,24 +3,41 @@ package computer.seoultech.diary.controller;
 import computer.seoultech.diary.network.Header;
 import computer.seoultech.diary.network.UserRequest;
 import computer.seoultech.diary.network.UserResponse;
-import computer.seoultech.diary.service.UserService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import computer.seoultech.diary.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/user")
+@RequestMapping("")
 public class UserController {
     private final UserService userService;
-    @PostMapping("/new")
-    public Long userRegister(@ModelAttribute Header<UserRequest> userRequest){  //회원가입 controller
-        return userService.save(userRequest);  //userService class에서 구현됨
+
+    @PostMapping("/user/new")
+    public void userRegister(@RequestBody UserRequest userRequest){  //회원가입 controller
+        userService.save(userRequest);  //userService class에서 구현됨
 
     }
+    @GetMapping("/users")
+    public Header<List<UserResponse>> getUsers(@PageableDefault Pageable pageable){  //user 리스트 , admin만 접근 가능
+        return userService.search(pageable);        //userService class에 구현되어 있음
+    }
 
+    @GetMapping("/user/{account}")
+    public Header<UserResponse> myPage(@PathVariable String account){  //계정으로 마이페이지 찾기
+        return userService.myPage(account); //userService class에 구현되어 있음
+    }
+    @PutMapping("/user/{account}")
+    public void update(@PathVariable String account,UserRequest userRequest){
+        userService.update(account,userRequest);
+    }
+    @DeleteMapping("/user/{id}")
+    public void delete(@PathVariable Long id){
+        userService.delete(id);
+    }
 }
