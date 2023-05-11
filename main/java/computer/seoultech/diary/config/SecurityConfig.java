9721fe996f01aa16e5f80e5ac8c33bccd91072e6
igh197 <57895194+igh197@users.noreply.github.com>
2023-05-11 21:15:47 +0900

    config revision

--- main/java/computer/seoultech/diary/config/SecurityConfig.java
+++ main/java/computer/seoultech/diary/config/SecurityConfig.java
@@ -2,15 +2,22 @@ package computer.seoultech.diary.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-@EnableWebSecurity
+import org.springframework.stereotype.Component;
+
+
+import java.util.Arrays;
+@Component
 @RequiredArgsConstructor
-public class SecurityConfig {  //WebSecurityAdapter class는 더이상 권장되지 않아 사용하지 않았음
+@EnableWebSecurity
+public class SecurityConfig {  //WebSecurityConfigurerAdapter class는 더이상 권장되지 않아 사용하지 않았음
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
@@ -22,33 +29,35 @@ public class SecurityConfig {  //WebSecurityAdapter class는 더이상 권장되
             Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
-                //get request
-                .requestMatchers(HttpMethod.GET,"/diarys","/diary/{id}","/diary/bookmarks","/diaryfiles","/diaryfile/{id}").hasAuthority("ROLE_USER")
-                .requestMatchers(HttpMethod.GET,"/user/{id}","/users").hasAuthority("ROLE_ADMIN")
                 //post request
                 .requestMatchers(HttpMethod.POST,"/user/new").permitAll()
-                .requestMatchers(HttpMethod.POST,"/diary/new","/image/new","/diaryfile/{id}").hasAuthority("ROLE_USER")
+                .requestMatchers(HttpMethod.POST,"/diary/new","/image/new","/diaryfile/{id}").hasRole("ROLE_USER")
+                //get request
+                .requestMatchers(HttpMethod.GET,"/login.html").permitAll()
+                .requestMatchers(HttpMethod.GET,"/diarys","/diary/{id}","/diary/bookmarks","/diaryfiles","/diaryfile/{id}").hasRole("USER")
+                .requestMatchers(HttpMethod.GET,"/user/{id}","/users").hasRole("USER")
                 //put request
-                .requestMatchers(HttpMethod.PUT,"/diary/{id}").hasAuthority("ROLE_USER")
-                .requestMatchers(HttpMethod.PUT,"/user/{id}").hasAuthority("ROLE_USER")
+                .requestMatchers(HttpMethod.PUT,"/diary/{id}").hasRole("USER")
+                .requestMatchers(HttpMethod.PUT,"/user/{id}").hasRole("USER")
                 //delete request
-                .requestMatchers(HttpMethod.DELETE,"/diary/{id}").hasAuthority("ROLE_USER")
-                .requestMatchers(HttpMethod.DELETE,"/user/{id}").hasAuthority("ROLE_ADMIN")
-                .requestMatchers(HttpMethod.DELETE,"/image/{id}").hasAuthority("ROLE_USER")
-
+                .requestMatchers(HttpMethod.DELETE,"/diary/{id}").hasRole("USER")
+                .requestMatchers(HttpMethod.DELETE,"/user/{id}").hasRole("USER")
+                .requestMatchers(HttpMethod.DELETE,"/image/{id}").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
+
                 .formLogin()
-                .loginPage("")
+                .loginPage("/login.html")
                 .usernameParameter("account") // 계정 ID
                 .passwordParameter("password") //비밀번호
                 .loginProcessingUrl("/login") //스프링 시큐리티가 제공하는 로그인 인증 기능
                 .defaultSuccessUrl("/diary/bookmarks")
-                .and()
-                .exceptionHandling()
-                .accessDeniedPage("/diarys")
-        ;
+                .and().logout().invalidateHttpSession(true).logoutSuccessUrl("/")
+                ;
+
+
 
         return http.build();
     }
+
 }
