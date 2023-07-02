5aeb134bf515f6766f24a13405de3985df363af3
Sminp <0102262@gmail.com>
2023-07-03 01:15:42 +0900

    박소민 깃허브 오류 해결

---
+++ main/resources/static/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as ServiceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
+import rootReducer, { rootSaga } from './modules';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
+
+sagaMiddleware.run(rootSaga);
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <React.Fragment>
+      <App />
+    </React.Fragment>
+  </Provider>
+);
+
+ServiceWorker.unregister();
