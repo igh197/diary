9271427505562b6fc8e047906fef6fb2a768e305
Sminp <0102262@gmail.com>
2023-07-03 01:15:57 +0900

    박소민 깃허브 오류 해결

---
+++ main/resources/static/src/modules/index.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import auth, {authSaga} from './auth';
+import loading from './loading';
+import { all } from 'redux-saga/effects';
+
+const rootReducer = combineReducers({
+  auth,
+  loading,
+});
+
+export function* rootSaga() {
+  yield all([authSaga()]);
+}
+
+export default rootReducer;
