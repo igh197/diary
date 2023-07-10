3bfb25087707e68078387253833d3b70a9c19b68
igh197 <57895194+igh197@users.noreply.github.com>
2023-07-10 13:43:51 +0900

    Add files via upload

--- main/java/computer/seoultech/diary/DiaryApplication.java
+++ main/java/computer/seoultech/diary/DiaryApplication.java
@@ -2,12 +2,25 @@ package computer.seoultech.diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+
 public class DiaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiaryApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
