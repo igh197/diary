025dcff262888bebce06a36be036ad890578a88e
Sminp <0102262@gmail.com>
2023-08-07 20:50:17 +0900

    박소민 태그 추가 및 API 연결 설정

---
+++ main/resources/static/src/containers/write/WriteActionButtonsContainer.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import WriteActionButton from '../../components/write/WriteActionButton';
+import { useDispatch, useSelector } from 'react-redux';
+import { writePost } from '../../modules/write';
+import { useNavigate } from 'react-router-dom';
+
+const WriteActionButtonContainer = ({ history }) => {
+  const dispatch = useDispatch();
+  const { title, body, tags, post, postError } = useSelector(({ write }) => ({
+    title: write.title,
+    body: write.body,
+    tags: write.tags,
+    post: write.post,
+    postError: write.postError,
+  }));
+
+  // 포스트 등록
+  const onPublish = () => {
+    dispatch(
+      writePost({
+        title,
+        body,
+        tags,
+      }),
+    );
+  };
+
+  const navigate = useNavigate();
+
+  // 취소
+  const onCancel = () => {
+    navigate(-1);
+  };
+
+  // 성공 혹은 실패 시 할 작업
+  useEffect(() => {
+    if (post) {
+      const { _id, user } = post;
+      navigate(`/@${user.account}/${_id}`);
+    }
+    if (postError) {
+      console.log(postError);
+    }
+  }, [navigate, post, postError]);
+  return <WriteActionButton onPublish={onPublish} onCancel={onCancel} />;
+};
+
+export default WriteActionButtonContainer;
--- main/resources/static/src/pages/WritePage.js
+++ main/resources/static/src/pages/WritePage.js
@@ -1,17 +1,21 @@
-import React from "react";
-import Editor from "../components/write/Editor";
-import Resposive from "../components/common/Responsive";
-import Header from "../components/base/Header";
+import React from 'react';
+import Resposive from '../components/common/Responsive';
+import Header from '../components/base/Header';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonContainer from '../containers/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
-    return (
-        <>
-        <Header/>
-    <Resposive>
-        <Editor/>
-    </Resposive>
-        </>
-);
+  return (
+    <>
+      <Header />
+      <Resposive>
+        <EditorContainer />
+        <TagBoxContainer />
+        <WriteActionButtonContainer />
+      </Resposive>
+    </>
+  );
 };
 
 export default WritePage;
