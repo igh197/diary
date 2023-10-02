f953914d60c1334f0938aec75a22d0df2da774a2
Sminp <0102262@gmail.com>
2023-10-02 20:42:02 +0900

    박소민 post api 수정중

--- main/resources/static/src/lib/api/posts.js
+++ main/resources/static/src/lib/api/posts.js
@@ -1,25 +1,28 @@
 import qs from 'qs';
 import client from './client';
 
-export const writePost = ({ title, body, tags }) =>
-  client.post('/api/posts', { title, body, tags });
+export const writePost = ({ title, body, tags, emotions }) =>
+  client.post('/api/diary/new', { title, body, tags, emotions });
 
-export const readPost = (id) => client.get(`/api/posts/${id}`);
+export const readPost = (id) => client.get(`/api/diary/${id}`);
 
-export const listPosts = ({ page, account, tag }) => {
+export const listPosts = ({ page, account, tag, emotion }) => {
+  // 조금만 더 알아보자
   const queryString = qs.stringify({
     page,
     account,
     tag,
+    emotion,
   });
-  return client.get(`/api/posts?${queryString}`);
+  return client.get(`/api/diarys?${queryString}`);
 };
 
-export const updatePost = ({ id, title, body, tags }) =>
-  client.patch(`/api/posts/${id}`, {
+export const updatePost = ({ id, title, body, tags, emotions }) =>
+  client.put(`/api/diary/${id}`, {
     title,
     body,
     tags,
+    emotions,
   });
 
-export const removePost = (id) => client.delete(`/api/posts/${id}`);
+export const removePost = (id) => client.delete(`/api/diary/${id}`);
