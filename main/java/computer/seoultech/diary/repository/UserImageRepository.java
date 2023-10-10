463262bb7d54cee086414ccab46db97328356d45
igh197 <57895194+igh197@users.noreply.github.com>
2023-10-10 16:32:58 +0900

    Add files via upload

--- main/java/computer/seoultech/diary/repository/UserImageRepository.java
+++ main/java/computer/seoultech/diary/repository/UserImageRepository.java
@@ -5,9 +5,12 @@ import computer.seoultech.diary.entity.UserImage;
 import computer.seoultech.diary.network.UserImageDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserImageRepository extends JpaRepository<UserImage, Long> {
 
     Optional<UserImage> findUserimageById(Long id);
+
+    List<UserImage> findAll();
 }
