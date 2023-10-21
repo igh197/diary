546af33087ddeaa1f7a98e20d6ff11cacefaefb8
igh197 <57895194+igh197@users.noreply.github.com>
2023-10-21 23:02:43 +0900

    Add files via upload

--- main/java/computer/seoultech/diary/controller/UserController.java
+++ main/java/computer/seoultech/diary/controller/UserController.java
@@ -2,6 +2,7 @@ package computer.seoultech.diary.controller;
 
 import computer.seoultech.diary.entity.User;
 import computer.seoultech.diary.network.Header;
+import computer.seoultech.diary.network.LoginDto;
 import computer.seoultech.diary.network.UserRequest;
 import computer.seoultech.diary.network.UserResponse;
 
@@ -24,27 +25,23 @@ public class UserController {
     private final UserService userService;
     private final UserRepository userRepository;
 
-    @PostMapping("/register")
+    @PostMapping("user/new")
     public void userRegister(@RequestBody UserRequest userRequest){  //회원가입 controller
         userService.save(userRequest);  //userService class에서 구현됨
 
     }
-    @PostMapping("/loginProc")
-    public User login(@RequestBody UserRequest userRequest){
-        return userService.login(userRequest);
-    }
-    @GetMapping("/users")
-    public Header<List<UserResponse>> getUsers(@PageableDefault Pageable pageable){  //user 리스트 , admin만 접근 가능
-        return userService.search(pageable);        //userService class에 구현되어 있음
+    @PostMapping("user/{account}")
+    public User myPage(@PathVariable String account){
+        return userService.loadUserByUsername(account);
     }
 
-    @GetMapping("/user/{id}")
-    public Header<UserResponse> myPage(@PathVariable Long id){  //계정으로 마이페이지 찾기
-        return userService.myPage(id); //userService class에 구현되어 있음
+    @PostMapping("/user")
+    public User login(@RequestBody LoginDto loginDto){  //계정으로 마이페이지 찾기
+        return userService.login(loginDto); //userService class에 구현되어 있음
     }
-    @PutMapping("/user/{id}")
-    public void update(@PathVariable Long id,@RequestBody UserRequest userRequest){
-        userService.update(id,userRequest);   //사용자 정보 수정
+    @PutMapping("/user/{account}")
+    public void update(@PathVariable String account,@RequestBody UserRequest userRequest){
+        userService.update(account,userRequest);   //사용자 정보 수정
     }
     @DeleteMapping("/user/{id}")
     public void delete(@PathVariable Long id){
